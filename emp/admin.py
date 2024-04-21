b975cd22ad573c9d7c21ae703a37edcb2ffc03e8
SIVA P <102237945+SivaECE@users.noreply.github.com>
2024-04-21 23:51:49 +0530

    Update admin.py
    Signed-off-by: SIVA P <102237945+SivaECE@users.noreply.github.com>

--- emp/admin.py
+++ emp/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
-from .models import Employee
+from .models import Employeedetails
 from .models import Salary
 from .models import CalendarEvent
 from .models import History
 
 # Register your models here.
  
-admin.site.register(Employee)
+admin.site.register(Employeedetails)
 admin.site.register(Salary)
 admin.site.register(CalendarEvent)
 admin.site.register(History)
